232b964d99ffb3da949f9d9341d06fef4ac87680
unknown <fernandopaivap7@gmail.com>
2025-12-04 14:30:06 +0100

    fix layout export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     "modista madrid",
     "arreglos express madrid",
     "arreglos de pantalones madrid",
-    "arreglos de vestidos madrid"
+    "arreglos de vestidos madrid",
   ],
   authors: [{ name: "Arreglos Express Madrid" }],
   creator: "Arreglos Express Madrid",
@@ -33,3 +33,36 @@ export const metadata: Metadata = {
     },
   },
 };
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { LanguageProvider } from "@/context/LanguageContext";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-body antialiased" suppressHydrationWarning={true}>
+        <LanguageProvider>
+          {children}
+          <Toaster />
+        </LanguageProvider>
+      </body>
+    </html>
+  );
+}
